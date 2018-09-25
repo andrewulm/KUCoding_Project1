ac1388de2e91d09a874067e30d6a0f9adbaca44c
Andrew Ulm <andrewulm@gmail.com>
2018-09-25 00:11:58 -0500

    Cleaned up

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,13 +1,11 @@
 // Nosh
-//
+// Author: Andrew Ulm
 
 const YUMMLY_APIKEY = 'ed390d594e8e1a62e504715e17a2943b';
 const YUMMLY_APPID = '368554d1';
 
-var searchTerm = 'chicken'
-var YUMMLY_URL = 'https://api.yummly.com/v1/api/recipes?_app_id=' + YUMMLY_APPID + '&_app_key=' + YUMMLY_APIKEY + '&q=' + searchTerm;
-
-console.log(YUMMLY_URL);
+let searchTerm = 'chicken';
+let YUMMLY_URL = 'https://api.yummly.com/v1/api/recipes?_app_id=' + YUMMLY_APPID + '&_app_key=' + YUMMLY_APIKEY + '&q=' + searchTerm;
 
 $.ajax({
     url: YUMMLY_URL,
