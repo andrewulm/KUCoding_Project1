5ab38f7001026b5efae428724afb65fe01d9e755
Eki <ekitaula@gmail.com>
2018-09-26 20:46:51 -0500

    idkwhatever

---
+++ assets/js/app.js
@@ -0,0 +1,23 @@
+// Nosh
+const YUMMLY_APIKEY = 'ed390d594e8e1a62e504715e17a2943b';
+const YUMMLY_APPID = '368554d1';
+
+var searchTerm = 'chicken'
+var YUMMLY_URL = 'https://api.yummly.com/v1/api/recipes?_app_id=' + YUMMLY_APPID + '&_app_key=' + YUMMLY_APIKEY + '&q=' + searchTerm;
+
+console.log(YUMMLY_URL);
+
+$.ajax({
+    url: YUMMLY_URL,
+    method: 'GET'
+}).then( function(res) {
+//create a variable named "noshb" equal to $("button");
+ var recipeButton = $("button")
+ recipeButton.addclass ("recipe-button recipe recipe-button-color");
+ recipeButton.attr("data-recipe", recipeButton );
+ recipeButton.text(recipe[recipe]);
+ $("#recipes").append(recipeButton)
+ 
+    console.log(res)
+
+});
