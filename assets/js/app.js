06b258f550f149029e429f103e985e6d93fbb99d
TSodo <whigs93@gmail.com>
2018-09-29 12:06:22 -0500

    JS work

--- assets/js/app.js
+++ assets/js/app.js
@@ -3,32 +3,44 @@
 
 $(document).ready(function () {
 
-    const YUMMLY_APIKEY = 'ed390d594e8e1a62e504715e17a2943b';
-    const YUMMLY_APPID = '368554d1';
+    
 
-    let searchTerms = [];
-    let YUMMLY_URL = 'https://api.yummly.com/v1/api/recipes?_app_id=' + YUMMLY_APPID + '&_app_key=' + YUMMLY_APIKEY + '&q=' + searchTerms;
-
-    $.ajax({
-        url: YUMMLY_URL,
-        method: 'GET'
-    }).then(function (res) {
-
-    });
-
-    $('#_addIngredient').on('click', function (event) {
+    
+    
+    $("#_addIngredient").on('click', function(event){
         event.preventDefault();
-
-        let ingredient = $('#_ingredient').val().trim();
-
-        searchTerms.push(ingredient);
-
-        $('#_ingredientList').html('<li>' + ingredient)
+        var searchTerms = $("#_ingredient").val().trim();
 
         console.log(searchTerms);
-
-        $('#_ingredient').val('');
+        var YUMMLY_APIKEY = 'ed390d594e8e1a62e504715e17a2943b';
+        var YUMMLY_APPID = '368554d1';
+        var query_URL = 'https://api.yummly.com/v1/api/recipes?_app_id=' + YUMMLY_APPID + '&_app_key=' + YUMMLY_APIKEY + '&q=' + searchTerms;
+        console.log(query_URL);
+        $.ajax({
+            url: query_URL,
+            method: 'GET'
+        })
+            .then(function (response) {
+                var matches = response;
+                console.log(matches);
+                for (var i = 0; i < matches.length; i++) {
+                   
+                        var ingredientDiv = $("<div>");
+                        var recipeName = matches[i].recipeName;
+                        console.log(recipeName);
+                        var p = $("<p>").text("Recipe: " + recipeName)
+                        var recipeImage = $("<img>");
+                        recipeImage.attr("src",matches[i].smallImageURLs);
+                        ingredientDiv.append(p);
+                        ingredientDiv.append(recipeImage);
+                        $("#_recipes").prepend(ingredientDiv);
+                    
+                    
+                }
+            })
 
     });
+
+
 
 });
