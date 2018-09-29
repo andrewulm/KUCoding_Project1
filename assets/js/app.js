2bdb72ae83ee9cbcafec0301fa0df996bd561d9a
Andrew Ulm <andrewulm@gmail.com>
2018-09-29 11:04:50 -0500

    Add to list function

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,18 +1,34 @@
 // Nosh
-
 // Author: Andrew Ulm
 
-const YUMMLY_APIKEY = 'ed390d594e8e1a62e504715e17a2943b';
-const YUMMLY_APPID = '368554d1';
+$(document).ready(function () {
 
-let searchTerm = 'chicken';
-let YUMMLY_URL = 'https://api.yummly.com/v1/api/recipes?_app_id=' + YUMMLY_APPID + '&_app_key=' + YUMMLY_APIKEY + '&q=' + searchTerm;
+    const YUMMLY_APIKEY = 'ed390d594e8e1a62e504715e17a2943b';
+    const YUMMLY_APPID = '368554d1';
 
-$.ajax({
-    url: YUMMLY_URL,
-    method: 'GET'
-}).then( function(res) {
- 
-    console.log(res)
+    let searchTerms = [];
+    let YUMMLY_URL = 'https://api.yummly.com/v1/api/recipes?_app_id=' + YUMMLY_APPID + '&_app_key=' + YUMMLY_APIKEY + '&q=' + searchTerms;
+
+    $.ajax({
+        url: YUMMLY_URL,
+        method: 'GET'
+    }).then(function (res) {
+
+    });
+
+    $('#_addIngredient').on('click', function (event) {
+        event.preventDefault();
+
+        let ingredient = $('#_ingredient').val().trim();
+
+        searchTerms.push(ingredient);
+
+        $('#_ingredientList').html('<li>' + ingredient)
+
+        console.log(searchTerms);
+
+        $('#_ingredient').val('');
+
+    });
 
 });
