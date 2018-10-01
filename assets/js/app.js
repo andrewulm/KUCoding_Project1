15d870980c2dd5699ee335708d13de64514faec5
Andrew Ulm <andrewulm@gmail.com>
2018-09-30 22:21:21 -0500

    Functionality work

--- assets/js/app.js
+++ assets/js/app.js
@@ -3,48 +3,57 @@
 
 $(document).ready(function () {
 
-    
+    let ingredients = [];
 
-    
-    
+    const YUMMLY_APIKEY = 'ed390d594e8e1a62e504715e17a2943b';
+    const YUMMLY_APPID = '368554d1';
+    var YUMMLY_URL = 'https://api.yummly.com/v1/api/recipes?_app_id=' + YUMMLY_APPID + '&_app_key=' + YUMMLY_APIKEY + '&q=' + ingredients;
+
+    function addIngredient(ingredient) {
+        ingredients.push(ingredient.toLowerCase() + '+');
+
+        $('#_ingredientList').append('<li>' + ingredient + '<button class="delete"> X </button>');
+        console.log(ingredients);
+
+    }
+
+    function getRecipes() {
+        $.ajax({
+            url: YUMMLY_URL,
+            method: 'GET'
+        }).then( function(res) {
+            console.log(res);
+        })
+    }
+
+    function showGetRecipesButton() {
+        if ( ingredients.length > 1 ) {
+            $('#_getRecipes').css('display', 'block');
+        } else {
+            $('#_getRecipes').css('display', 'none');
+        }
+    }
+
+
     $("#_addIngredient").on('click', function(event){
         event.preventDefault();
-        var searchTerms = $("#_ingredient").val().trim();
+        showGetRecipesButton();
 
-        console.log(searchTerms);
-        var YUMMLY_APIKEY = 'ed390d594e8e1a62e504715e17a2943b';
-        var YUMMLY_APPID = '368554d1';
-        var query_URL = 'https://api.yummly.com/v1/api/recipes?_app_id=' + YUMMLY_APPID + '&_app_key=' + YUMMLY_APIKEY + '&q=' + searchTerms;
-        console.log(query_URL);
-        $.ajax({
-            url: query_URL,
-            method: 'GET'
-        })
-            .then(function (response) {
-                var results = response;
-                console.log(results);
-                for (var i = 0; i < response.matches.length; i++) {
-                    
-                        var ingredientDiv = $("<div class = 'col-sm-6' >");
-                        var recipeName = response.matches[i].recipeName;
-                        console.log(recipeName);
-                        var p = $("<p>").text("Recipe: " + recipeName)
-                        var recipeImage = $("<img>");
-                        recipeImage.attr("src",response.matches[i].smallImageUrls);
-                        console.log(recipeImage);
-                        $("a").attr("href", "http://www.yummly.com/recipe/" + response.matches[i].id);
-                        ingredientDiv.append("<a href = 'www.yummly.com/recipe/'" + response.matches[i].id + '>' + "<p> Recipe: " + recipeName + "</p>" +"</a>");
-                        ingredientDiv.append(recipeImage);
-                        
-                        
-                        $("#_recipes").prepend(ingredientDiv);
-                    
-                    
-                }
-            })
+        let ingredient = $("#_ingredient").val().trim();
 
+        if ( ingredient === '') {
+            console.log('empty');
+        } else {
+            addIngredient(ingredient);
+            $('#_ingredient').val('');
+        }
     });
 
+    $('#_getRecipes').on('click', function(event){
+        event.preventDefault();
 
+        YUMMLY_URL = 'https://api.yummly.com/v1/api/recipes?_app_id=' + YUMMLY_APPID + '&_app_key=' + YUMMLY_APIKEY + '&q=' + ingredients;
 
+        getRecipes();
+    });
 });
